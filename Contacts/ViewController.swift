77fc77d5d0eb973d3b1cfe943fded4771efab004
Collin Hemeltjen <collinhemeltjen@macbook-pro-collin.local>
2019-02-19 18:50:52 +0100

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Collin Hemeltjen on 19/02/2019.
+//  Copyright © 2019 Collin Hemeltjen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
